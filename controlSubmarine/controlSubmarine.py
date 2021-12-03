150d672e97542e07884068614ae7eca6590c8ce7
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-12-03 20:51:19 +0100

    day 2

---
+++ controlSubmarine/controlSubmarine.py
@@ -0,0 +1,75 @@
+from Mixins.mixins import read_file
+
+class Submarine:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def __str__(self):
+        return(f"({self.x} ; {self.y})")
+
+    def go_up(self, x: int):
+        self.y-=x
+    
+    def go_down(self, x: int):
+        self.y+=x
+
+    def go_forward(self, x: int):
+        self.x += x
+
+
+def main():
+    dataset = read_file("./controlSubmarine/dataset1.csv")
+    sub = Submarine()
+    with open("./controlSubmarine/answer1.csv","w") as f:
+        for line in dataset:
+            line = line.split(' ')
+            if line[0] == 'forward':
+                sub.go_forward(int(line[1]))
+            elif line[0] == 'down':
+                sub.go_down(int(line[1]))
+            elif line[0] == 'up':
+                sub.go_up(int(line[1]))
+            else:
+                print('ERROR')
+            f.write(f'actualisation: {line[0]} -> {line[1]}\tnouvelle position: {sub}\n')
+        f.write(f'dernière position: {sub} réponse au problème: {sub.x * sub.y}')
+    return sub.x * sub.y
+
+
+class SubmarineAim(Submarine):
+    def __init__(self):
+        super().__init__()
+        self.aim = 0
+
+    def __str__(self):
+        return(f"({self.x} ; {self.y}; {self.aim})")
+
+    def go_up(self, x: int):
+        self.aim-=x
+    
+    def go_down(self, x: int):
+        self.aim+=x
+
+    def go_forward(self, x: int):
+        self.x += x
+        self.y += x * self.aim
+
+def main2():
+    dataset = read_file("./controlSubmarine/dataset1.csv")
+    sub = SubmarineAim()
+
+    with open("./controlSubmarine/answer2.csv","w") as f:
+        for line in dataset:
+            line = line.split(' ')
+            if line[0] == 'forward':
+                sub.go_forward(int(line[1]))
+            elif line[0] == 'down':
+                sub.go_down(int(line[1]))
+            elif line[0] == 'up':
+                sub.go_up(int(line[1]))
+            else:
+                print('ERROR')
+            f.write(f'actualisation: {line[0]} -> {line[1]} {sub.aim}\tnouvelle position: {sub}\n')
+        f.write(f'\ndernière position: {sub} réponse au problème: {sub.x * sub.y}')
+    return sub.x * sub.y
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Submarine import elvesSubmarine
+from controlSubmarine import controlSubmarine
 
 # day 1
-print(f"réponse: la profondeur augmente {elvesSubmarine.main()} fois sur l'échantillon.")
-print(f"réponse: la profondeur augmente {elvesSubmarine.main2()} fois sur l'échantillon avec une fenêtre de 3 mesures")
+# print(f"réponse: la profondeur augmente {elvesSubmarine.main()} fois sur l'échantillon.")
+# print(f"réponse: la profondeur augmente {elvesSubmarine.main2()} fois sur l'échantillon avec une fenêtre de 3 mesures")
 
 # day 2
+# print(f"réponse: la multiplication de la profondeur finale par l'avance finale est de {controlSubmarine.main()}")
+print(f"réponse: la multiplication de la profondeur finale par l'avance finale est de {controlSubmarine.main2()}")
